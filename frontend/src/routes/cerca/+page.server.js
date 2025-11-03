e5023e97dfef6404efff8e2e86d4c8b917ae11ba
lucabunino <66553738+lucabunino@users.noreply.github.com>
2025-11-03 12:03:26 +0100

    searchBoost

--- frontend/src/routes/cerca/+page.server.js
+++ frontend/src/routes/cerca/+page.server.js
@@ -1,16 +1,25 @@
-import { getExploreTopics, getLiveTopics, getAuthors, getExplore, getLive  } from '$lib/utils/sanity';
+import { getExploreTopics, getLiveTopics, getAuthors, getExplore, getLive } from '$lib/utils/sanity';
 import { error } from '@sveltejs/kit';
+
 export async function load({ url }) {
-	const exploreTopics = await getExploreTopics();
-	const liveTopics = await getLiveTopics();
 	const searchMedias = url.searchParams.getAll('media');
 	const searchFormats = url.searchParams.get('format');
 	const searchCity = url.searchParams.get('city');
 	const searchTopic = url.searchParams.get('topic');
 	const searchString = url.searchParams.get('search');
-	const authors = await getAuthors(searchString);
-	const explore = await getExplore(searchMedias, searchTopic, searchString);
-	const live = await getLive(searchFormats, searchCity, searchTopic, searchString);
+	const [
+		exploreTopics,
+		liveTopics,
+		authors,
+		explore,
+		live
+	] = await Promise.all([
+		getExploreTopics(),
+		getLiveTopics(),
+		getAuthors(searchString),
+		getExplore(searchMedias, searchTopic, searchString),
+		getLive(searchFormats, searchCity, searchTopic, searchString)
+	]);
 	if (exploreTopics && liveTopics && authors && explore && live) {
 		return {
 			searchMedias,
