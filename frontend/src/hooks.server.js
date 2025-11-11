6885ab9aa20ccd87fbac6d12c56677ba0f269fb9
lucabunino <66553738+lucabunino@users.noreply.github.com>
2025-11-11 11:07:03 +0100

    redirectTest

--- frontend/src/hooks.server.js
+++ frontend/src/hooks.server.js
@@ -2,6 +2,15 @@
 import { dev } from '$app/environment';
 
 export async function handle({ event, resolve }) {
+	const redirects = new Map([
+		['/podcast-la-ricerca-della-balena-bianca', '/esplora/la-ricerca-della-balena-bianca'],
+	]);
+
+	const target = redirects.get(event.url.pathname);
+	if (target) {
+		return Response.redirect(target, 308);
+	}
+
 	if (dev && event.url.pathname === '/.well-known/appspecific/com.chrome.devtools.json') {
 		return new Response(undefined, { status: 404 });
 	}
